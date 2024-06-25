ab8b4da295c37555550acb4dd9b5eed92b3cac90
Liam <flanlc@farmingdale.edu>
2024-06-25 19:17:15 -0400

    FXML Main Menu plus Id's assigned

---
+++ src/main/java/com/example/javafxoop/MainMenuController.java
@@ -0,0 +1,63 @@
+package com.example.javafxoop;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+
+public class MainMenuController {
+    @FXML
+    private TextField firstNameField;
+    @FXML
+    private TextField lastNameField;
+    @FXML
+    private TextField deptField;
+    @FXML
+    private TextField majorField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField imgURLField;
+    @FXML
+    private Button clearButton;
+    @FXML
+    private Button addButton;
+    @FXML
+    private Button deleteButton;
+    @FXML
+    private Button editButton;
+    @FXML
+    private TableView tableView;
+    @FXML
+    private TableColumn firstNameCol;
+    @FXML
+    private TableColumn lastNameCol;
+    @FXML
+    private TableColumn deptCol;
+    @FXML
+    private TableColumn majorCol;
+    @FXML
+    private TableColumn emailCol;
+    @FXML
+    private TableColumn idCol;
+    @FXML
+    private MenuBar menuBar;
+    @FXML
+    private Menu menuFile;
+    @FXML
+    private Menu menuEdit;
+    @FXML
+    private Menu menuTheme;
+    @FXML
+    private Menu menuHelp;
+    @FXML
+    private MenuItem menuItemClose;
+    @FXML
+    private MenuItem menuItemDelete;
+    @FXML
+    private MenuItem menuItemEdit;
+    @FXML
+    private MenuItem menuItemAbout;
+    @FXML
+    private ImageView imageView;
+
+}
